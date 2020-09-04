8d1a3a843cacacb9e619c098e3e1319de534bd47
Joel Burton <joel@joelburton.com>
2020-09-04 13:00:56 -0700

    Test

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,3 +1,10 @@
-struct MyLibrary {
-    var text = "Hello, World!"
+public class SerialMachine {
+    private var counter = 0
+    public func next(inc:Int = 1) -> Int {
+        counter += inc
+        return counter
+    }
+    public init() {
+        counter = 0;
+    }
 }
